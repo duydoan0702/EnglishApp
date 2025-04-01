2aca64908e0a0f22237181c0f2c6acb25b0135ed
Vuanh47 <daovuanh2207@gmail.com>
2025-04-01 09:32:56 +0700

    Giao dien home, profie, vocabulary, start

---
+++ app/src/main/java/com/example/learnenglishapp/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.learnenglishapp
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.d("MainActivity", "Trước khi gọi setContentView")
+        setContentView(R.layout.activity_main)
+        Log.d("MainActivity", "Sau khi gọi setContentView")
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as? NavHostFragment
+
+        val navController = navHostFragment?.navController
+
+        if (navController != null) {
+            val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+            NavigationUI.setupWithNavController(bottomNavigationView, navController)
+            Log.d("MainActivity", "BottomNavigationView đã được thiết lập với NavController")
+        } else {
+            Log.e("MainActivity", "NavController not found!")
+        }
+    }
+}
+
+
+
