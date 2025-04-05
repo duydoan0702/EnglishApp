b4d59b16a50c0d15e029b38beff4203298cc57a5
Vuanh47 <daovuanh2207@gmail.com>
2025-04-05 16:56:32 +0700

    Giao dien Nghe lặp lại và viết lại

---
+++ app/src/main/java/com/example/learnenglishapp/home/ConversationFragment.kt
@@ -0,0 +1,41 @@
+package com.example.learnenglishapp.home
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.example.learnenglishapp.ListenAndRepeatListActivity
+import com.example.learnenglishapp.ListenAndWriteListActivity
+import com.example.learnenglishapp.R
+
+class ConversationFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Gắn layout của fragment vào
+        return inflater.inflate(R.layout.fragment_conversation, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Gắn sự kiện cho button
+        val btnWrite = view.findViewById<Button>(R.id.button2)
+        val btnRepeat = view.findViewById<Button>(R.id.button3)
+        btnWrite.setOnClickListener {
+            // Mở LessonDetailActivity
+            val intent = Intent(requireContext(), ListenAndWriteListActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnRepeat.setOnClickListener{
+            val intent =  Intent(requireContext(), ListenAndRepeatListActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
